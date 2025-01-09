a96d6282e9398654c35eb7b16784d5cc97f67038
Crosslywere <ogboruj@outlook.com>
2025-01-09 21:58:06 +0100

    implemented shaders

---
+++ ShaderTool/src/Shader.cpp
@@ -0,0 +1,177 @@
+#include <Shader.hpp>
+
+#include <glad/glad.h>
+
+#include <fstream>
+#include <sstream>
+
+#define INFO_LOG_LENGTH 1024
+
+inline unsigned int FrameBufferShader::s_DefaultVertexShader = 0;
+
+inline const char* frameBufferVertexSourceDefault =
+R"(#version 420 core
+layout (location = 0) in vec2 Pos;
+layout (location = 1) in vec2 TexCoord;
+out vec2 aPos;
+out vec2 aTexCoord;
+void main() {
+	aPos = Pos;
+	aTexCoord = TexCoord;
+	gl_Position = vec4(Pos, 0.0, 1.0);	
+})";
+
+static GLuint LoadShaderFromFile(const std::string& filePath, GLenum type) {
+	std::ifstream fs;
+	fs.open(filePath);
+	if (fs.is_open()) {
+		GLuint shader = glCreateShader(type);
+		std::stringstream ss;
+		ss << fs.rdbuf();
+		std::string srcStr = ss.str();
+		const char* src = srcStr.c_str();
+		glShaderSource(shader, 1, &src, 0);
+		return shader;
+	}
+	std::printf("Unable to open shader '%s'\n", filePath.c_str());
+	return 0;
+}
+
+static std::string CheckShaderCompileError(GLuint shader) {
+	int success;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		char infoLog[INFO_LOG_LENGTH];
+		glGetShaderInfoLog(shader, INFO_LOG_LENGTH, nullptr, infoLog);
+#ifdef DEBUG
+		std::printf("%s\n", infoLog);
+#endif
+		return infoLog;
+	}
+	return "";
+}
+
+Shader::Shader(const std::string& vertexShaderFile, const std::string& fragmentShaderFile)
+	: m_VertexFilePath(vertexShaderFile), m_FragmentFilePath(fragmentShaderFile) {
+	Reload();
+}
+
+Shader::~Shader() {
+	if (m_Program != 0) {
+		glDeleteProgram(m_Program);
+		m_Program = 0;
+	}
+}
+
+void Shader::Bind() const {
+	glUseProgram(m_Program);
+}
+
+void Shader::Reload() {
+	if (m_Program != 0)
+		glDeleteProgram(m_Program);
+	m_Program = glCreateProgram();
+	auto vs = LoadShaderFromFile(m_VertexFilePath, GL_VERTEX_SHADER);
+	auto fs = LoadShaderFromFile(m_FragmentFilePath, GL_FRAGMENT_SHADER);
+	if (vs == 0 || fs == 0) {
+		m_ErrorStr = "Unable to load file(s)";
+		return;
+	}
+	glCompileShader(vs);
+	m_VertexErrorStr = CheckShaderCompileError(vs);
+	glCompileShader(fs);
+	m_FragmentErrorStr = CheckShaderCompileError(fs);
+	if (IsVertexError() || IsFragmentError()) {
+		return;
+	}
+	glAttachShader(m_Program, vs);
+	glAttachShader(m_Program, fs);
+	glLinkProgram(m_Program);
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+}
+
+void Shader::SetUniform1f(const std::string& name, float v) {
+	int loc = GetUniformLocation(name);
+	if (loc >= 0) {
+		Bind();
+		glUniform1f(loc, v);
+	}
+}
+
+void Shader::SetUniform2f(const std::string& name, float v1, float v2) {
+	int loc = GetUniformLocation(name);
+	if (loc >= 0) {
+		Bind();
+		glUniform2f(loc, v1, v2);
+	}
+}
+
+void Shader::SetUniform3f(const std::string& name, float v1, float v2, float v3) {
+	int loc = GetUniformLocation(name);
+	if (loc >= 0) {
+		Bind();
+		glUniform3f(loc, v1, v2, v3);
+	}
+}
+
+int Shader::GetUniformLocation(const std::string& name) {
+	if (m_UniformMap.find(name) == m_UniformMap.end()) {
+		int loc = glGetUniformLocation(m_Program, name.c_str());
+		m_UniformMap.insert(std::make_pair(name, loc));
+	}
+	return m_UniformMap.at(name);
+}
+
+void FrameBufferShader::Init() {
+	if (s_DefaultVertexShader == 0) {
+		s_DefaultVertexShader = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(s_DefaultVertexShader, 1, &frameBufferVertexSourceDefault, nullptr);
+		glCompileShader(s_DefaultVertexShader);
+	}
+}
+
+void FrameBufferShader::Deinit() {
+	if (s_DefaultVertexShader != 0) {
+		glDeleteShader(s_DefaultVertexShader);
+	}
+}
+
+FrameBufferShader::FrameBufferShader(const std::string& fragmentFilePath) : m_FilePath(fragmentFilePath) {
+	Reload();
+}
+
+void FrameBufferShader::Reload() {
+	if (m_Program != 0)
+		glDeleteProgram(m_Program);
+	m_Program = glCreateProgram();
+	glAttachShader(m_Program, s_DefaultVertexShader);
+	auto fs = LoadShaderFromFile(m_FilePath, GL_FRAGMENT_SHADER);
+	if (fs == 0) {
+		m_ErrorStr = "Unable to load file";
+		return;
+	}
+	glCompileShader(fs);
+	m_ErrorStr = CheckShaderCompileError(fs);
+	if (IsError()) {
+		return;
+	}
+	glAttachShader(m_Program, fs);
+	glLinkProgram(m_Program);
+	glDeleteShader(fs);
+	m_UniformMap.clear();
+}
+
+void FrameBufferShader::SetTime(float time) {
+	SetUniform1f("uTime", time);
+}
+
+void FrameBufferShader::SetResolution(int width, int height) {
+	SetUniform2f("uResolution", width, height);
+}
+
+void FrameBufferShader::SetMousePos(float mouseX, float mouseY) {
+	SetUniform2f("uMousePos", mouseX, mouseY);
+}
+
+
---
+++ ShaderTool/src/Shader.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+class Shader {
+public:
+	Shader() = default;
+	Shader(const Shader&) = default;
+	Shader(Shader&&) = default;
+	Shader& operator=(const Shader&) = default;
+	Shader(const std::string& vertexShader, const std::string& fragmentShader);
+	~Shader();
+	void Bind() const;
+	virtual void Reload();
+	void SetUniform1f(const std::string& name, float v);
+	void SetUniform2f(const std::string& name, float v1, float v2);
+	void SetUniform3f(const std::string& name, float v1, float v2, float v3);
+	inline bool IsVertexError() const { return !m_VertexErrorStr.empty(); }
+	inline const std::string_view& GetVertexError() const { return m_VertexErrorStr; }
+	inline bool IsFragmentError() const { return !m_FragmentErrorStr.empty(); }
+	inline const std::string_view& GetFragmentError() const { return m_FragmentErrorStr; }
+	inline bool IsError() const { return !m_ErrorStr.empty(); }
+	inline const std::string_view& GetErrorStr() const { return m_ErrorStr; }
+protected:
+	int GetUniformLocation(const std::string& name);
+	unsigned int m_Program{};
+	std::string m_ErrorStr{};
+	std::unordered_map<std::string, int> m_UniformMap;
+private:
+	std::string m_VertexFilePath{};
+	std::string m_VertexErrorStr{};
+	std::string m_FragmentFilePath{};
+	std::string m_FragmentErrorStr{};
+};
+
+class FrameBufferShader : public Shader {
+public:
+	static void Init();
+	static void Deinit();
+	FrameBufferShader() = default;
+	FrameBufferShader(const FrameBufferShader&) = default;
+	FrameBufferShader(FrameBufferShader&&) = default;
+	FrameBufferShader& operator=(const FrameBufferShader&) = default;
+	FrameBufferShader(const std::string& filePath);
+	virtual void Reload() override;
+	void SetTime(float time);
+	void SetResolution(int width, int height);
+	void SetMousePos(float mouseX, float mouseY);
+private:
+	std::string m_FilePath{};
+	static unsigned int s_DefaultVertexShader;
+};
